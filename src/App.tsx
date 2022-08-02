9ffbf3cb2e689f7cba58df5623b4b8bfa93ac06a
Anaid Candido <anaid.candido@gmail.com>
2022-08-02 12:23:22 +0100

    pass JSON data to a JSX element

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-import episodes from './episodes.json' 
+import episodes from './episodes.json'
 
+
+interface IShorterEpisode {
+  id: number;
+  name: string;
+  season: number;
+  number: number;
+  airdate: string;
+  airtime: string;
+  image: {
+    original: string;
+  };
+  summary: string;
+}
 
 function App(): JSX.Element {
-return <h1>{episodes.length}</h1>;
+  return (
+    <div className='App'>
+      <div className="episodesList" >
+        {episodes.map(structureEpisode)}
+      </div>
+    </div>
+  )
+}
+
+function structureEpisode(episodeData: IShorterEpisode) {
+  return <div
+    key={episodeData.id}>
+    episode name: {episodeData.name}
+    {episodeData.summary}
+  </div>
 }
 
 export default App;
-
+console.log(`Imported ${episodes.length} episode(s)`);
 //
 //import { greet } from "./utils/greet";
 //
