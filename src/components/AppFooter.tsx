842291a8be18aa510c0c50532630df2727994e90
Anaid Candido <anaid.candido@gmail.com>
2022-08-03 13:11:46 +0100

    initial comit

---
+++ src/components/AppFooter.tsx
@@ -0,0 +1,10 @@
+export default function AppFooter(): JSX.Element {
+  return (
+    <footer>
+      <p>
+        All images used in this app come from:{" "}
+        <a href="https://tvmaze.com/"> TV Maze </a>{" "}
+      </p>
+    </footer>
+  );
+}
---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,7 @@
+export default function AppHeader(): JSX.Element {
+  return (
+    <header>
+      <h1>Game of Thrones Episodes List</h1>
+    </header>
+  );
+}
