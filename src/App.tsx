bca08fb3264cded96516b45ea735a113b0d42c69
Anaid Candido <anaid.candido@gmail.com>
2022-08-03 13:44:23 +0100

    added searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import EpisodesList from "./components/EpisodesList";
 import AppFooter from "./components/AppFooter";
 import AppHeader from "./components/AppHeader";
+import SearchBar from "./components/SearchBar";
 
 function App(): JSX.Element {
   return (
     <>
       <AppHeader />
+      <SearchBar />
       <EpisodesList />
       <AppFooter />
     </>
