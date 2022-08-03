3b96432248a455c375b039a4558cb726d9cd7a9e
Anaid Candido <anaid.candido@gmail.com>
2022-08-03 16:26:03 +0100

    formatted code

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 //import episodes from "../episodes.json"
 
-
 /*interface IShorterEpisode {
     id: number;
     name: string;
@@ -17,19 +16,20 @@ import { useState } from "react";
   }
   */
 
-
 export default function SearchBar(): JSX.Element {
-const [searchEpisode, setSearchEpisode] = useState("")
+  const [searchEpisode, setSearchEpisode] = useState("");
   return (
     <div>
-  <input type="text" placeholder="search episode here!" onChange={event => (setSearchEpisode(event.target.value))}/>
-  {console.log("search value is", searchEpisode)}
-  {console.log("setsearch value is", setSearchEpisode)}
-  </div>
-  )
+      <input
+        type="text"
+        placeholder="search episode here!"
+        onChange={(event) => setSearchEpisode(event.target.value)}
+      />
+      {console.log("search value is", searchEpisode)}
+      {console.log("setsearch value is", setSearchEpisode)}
+    </div>
+  );
 }
-
-
 
 /* function findOneEpisode(oneEpisode: IShorterEpisode): boolean {
     return (oneEpisode.name.toLowerCase().includes(searchEpisode.toLowerCase()))
