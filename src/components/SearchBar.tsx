611aae6eb3b6e420f42ddf26269938ff6e5cc2a9
Anaid Candido <anaid.candido@gmail.com>
2022-08-03 16:25:27 +0100

    add useState values

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,3 +1,38 @@
-export default function searchBar(): JSX.Element {
-  return <input type="text" placeholder="search episode here!" />;
+import { useState } from "react";
+//import episodes from "../episodes.json"
+
+
+/*interface IShorterEpisode {
+    id: number;
+    name: string;
+    season: number;
+    number: number;
+    airdate: string;
+    airtime: string;
+    image: {
+      original: string;
+      medium: string;
+    };
+    summary: string;
+  }
+  */
+
+
+export default function SearchBar(): JSX.Element {
+const [searchEpisode, setSearchEpisode] = useState("")
+  return (
+    <div>
+  <input type="text" placeholder="search episode here!" onChange={event => (setSearchEpisode(event.target.value))}/>
+  {console.log("search value is", searchEpisode)}
+  {console.log("setsearch value is", setSearchEpisode)}
+  </div>
+  )
 }
+
+
+
+/* function findOneEpisode(oneEpisode: IShorterEpisode): boolean {
+    return (oneEpisode.name.toLowerCase().includes(searchEpisode.toLowerCase()))
+    }
+const filteredEpisodes = episodes.filter(findOneEpisode)
+*/
