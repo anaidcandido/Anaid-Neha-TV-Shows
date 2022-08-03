98d60f190ac7639794aed8575f004e8801d8f193
Anaid Candido <anaid.candido@gmail.com>
2022-08-03 13:43:45 +0100

    changed string concatenation

--- src/utils/seasonEpisodeCode.ts
+++ src/utils/seasonEpisodeCode.ts
@@ -7,15 +7,15 @@ export default function seasonEpisodeCode(props: episodeSeasonData): string {
   let seasonCode = "";
   let episodeCode = "";
   if (props.season >= 10) {
-    seasonCode = "S" + props.season;
+    seasonCode = `S${props.season}`;
   } else {
-    seasonCode = "S" + "0" + props.season;
+    seasonCode = `S0${props.season}`;
   }
 
   if (props.number >= 10) {
-    episodeCode = "E" + props.number;
+    episodeCode = `E${props.number}`;
   } else {
-    episodeCode = "E" + "0" + props.number;
+    episodeCode = `E0${props.number}`;
   }
   return seasonCode + episodeCode;
 }
