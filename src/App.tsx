a9e173b3235be4b6770a5b0424db99d8040a13d4
neha <neha.vaze@oxfordbusinessreview.org>
2022-08-04 16:56:37 +0100

    moved components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,49 @@
-import EpisodesList from "./components/EpisodesList";
-import AppFooter from "./components/AppFooter";
+//import EpisodesList from "./components/EpisodesList";
+import episodes from './episodes.json'
 import AppHeader from "./components/AppHeader";
-import SearchBar from "./components/SearchBar";
+import StructureEpisode from "./components/StructureEpisode";
+import { useState } from 'react'
+import AppFooter from "./components/AppFooter";
+
+interface IShorterEpisode {
+  id: number;
+  name: string;
+  season: number;
+  number: number;
+  airdate: string;
+  airtime: string;
+  image: {
+    original: string;
+    medium: string;
+  };
+  summary: string;
+}
+
 
 function App(): JSX.Element {
+  const episodesList = episodes.map(StructureEpisode)
+  const [searchItem, setSearchItem] = useState('')
   return (
     <>
       <AppHeader />
-      <SearchBar />
-      <EpisodesList />
+      {episodesList}
+      <input
+        type="text"
+        placeholder="search episode here!"
+        onChange={(event) => setSearchItem(event.target.value)}
+      />
+      {episodesList.filter(findEpisodes(episodes, searchItem))}
       <AppFooter />
     </>
   );
+}
+
+function findEpisodes(oneEpisode: IShorterEpisode, searchItem: string): boolean{
+  if(oneEpisode.name.toLowerCase().includes(searchItem.toLowerCase())){
+    return true 
+  } else {
+    return false 
+  }
 }
 
 export default App;
